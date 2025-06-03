27f3190f2cd550d6103b180bcea5679e0e080b23
abkulaka <bilguunmonkhbaatar@gmail.com>
2025-06-03 11:02:34 +0800

    WIP: local changes before pulling

---
+++ Motorcycle.java
@@ -0,0 +1,10 @@
+
+class Motorcycle extends Vehicle {
+    @Override
+    public void startEngine() {
+      System.out.println("Motorcycle: Starting the engine...");
+    }
+    public void stopEngine() {
+      System.out.println("Motorcycle: Stopping the engine...");
+    }
+  } 
