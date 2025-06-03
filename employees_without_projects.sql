ccfdd1f7d337095c320cfcd9ccd27c83650116a7
abkulaka <158141104+abkulaka@users.noreply.github.com>
2025-06-03 12:23:50 +0800

    Update and rename employees_without_projects.sql to Employee.cpp

--- employees_without_projects.sql
+++
@@ -1,9 +0,0 @@
-SELECT e.cpf, e.enome, d.dnome
-FROM empregados e
-JOIN departamentos d ON e.dnumero = d.dnumero
-WHERE e.cpf NOT IN (
-    SELECT t.cpf_emp
-    FROM trabalha t
-)
-ORDER BY e.cpf;
-
